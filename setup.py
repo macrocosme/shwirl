566f0548a353c077b65b4405ea13ed5c5c8d6c2f
macrocosme <danyvohl@hotmail.com>
2017-06-25 18:39:41 +1000

    Adding dependencies to setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.2',
+    version='0.1.3',
 
     description='Meaningful colouring of spectral cube data with volume rendering',
     long_description=long_description,
@@ -106,7 +106,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['astropy', 'PyOpenGL'],
+    install_requires=['scipy', 'numpy', 'astropy', 'PyOpenGL', 'six'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
